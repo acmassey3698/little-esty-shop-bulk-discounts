d8f7c07eb8638704b1a1a2b6ee7ae016c2f4cad2
Andrew Massey <acmassey@ncsu.edu>
2021-09-23 20:43:39 -0400

    Add stronger validations and create actions in the merchant_discounts controller

--- app/controllers/merchant_discounts_controller.rb
+++ app/controllers/merchant_discounts_controller.rb
@@ -7,4 +7,28 @@ class MerchantDiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @discount = Discount.find(params[:id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = Discount.new
+  end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    discount = merchant.discounts.create(discount_params)
+
+    if discount.save
+      redirect_to merchant_discounts_path(merchant)
+    else
+      redirect_to new_merchant_discount_path(merchant)
+      flash[:notice] = "Discount not created. Invalid Input"
+    end 
+  end
+
+
+
+  private
+  def discount_params
+    params.permit(:percentage, :threshold)
+  end
 end
