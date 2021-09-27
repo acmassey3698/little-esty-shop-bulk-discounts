d7507360168c834635b100f76a6b2a3674c4321d
Andrew Massey <acmassey@ncsu.edu>
2021-09-27 11:09:18 -0400

    Add new assocation to the Invoice Item model

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,6 +1,8 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+  has_many :merchants, through: :item
+  has_many :discounts, through: :merchants
 
   enum status: {
     pending: 0,
